52076ad2a5609ef475231fc7263dfc9249d9e305
Kai Zhao <zhaokaijia@gmail.com>
2023-01-19 20:29:20 -0600

    add pywt for c++ example

--- utilities/pywt_test.cpp
+++ utilities/pywt_test.cpp
@@ -7,41 +7,71 @@
 #include <iostream>
 #include "sperr_helper.h"
 namespace py = pybind11;
-int main(int argc, char* argv[])
+//py::module_ pywavelet;
+
+template <class T>
+void wavelet(T* data, std::vector<size_t>& dims)
 {
   auto startT = std::chrono::high_resolution_clock::now();
-  std::string HOME = "/Users/kzhao/";
-  py::scoped_interpreter guard{};
-
-  // append source dir to sys.path, and python interpreter would find your custom python file
-  py::module_::import("sys").attr("path").attr("append")(HOME + "/code/sperr/utilities/");
-  auto pycode = py::module_::import("pywt_wrapper");
-
+  auto pywavelet = py::module_::import("pywt_wrapper");
   auto endT = std::chrono::high_resolution_clock::now();
-  std::cout << "py prepare env time: " << (std::chrono::duration<double>(endT - startT)).count() << std::endl;
-
-  auto ori_data = sperr::read_whole_file<float>(HOME + "/data/hurricane-100x500x500/Uf48.bin.dat");
-
+  std::cout << "Time for import python module: " << (std::chrono::duration<double>(endT - startT)).count()
+            << std::endl;
 
   // wavelet
   startT = std::chrono::high_resolution_clock::now();
-  py::array_t<float> ori_data_py({100, 500, 500}, ori_data.data());
-  py::array_t<float> dwt_data = pycode.attr("dwt")(ori_data_py, "sym13");
-  auto dwt_structure = pycode.attr("dwt_structure")().cast<std::string>();
+  py::array_t<float> ori_data_py(dims, data);
+  py::array_t<float> dwt_data = pywavelet.attr("dwt")(ori_data_py, "sym13");
+  auto dwt_structure = pywavelet.attr("dwt_structure")().cast<std::string>();
   endT = std::chrono::high_resolution_clock::now();
   std::cout << "Time for wavelet: " << (std::chrono::duration<double>(endT - startT)).count()
             << std::endl;
 
   // reverse wavelet
   startT = std::chrono::high_resolution_clock::now();
-  py::array_t<float> idwt_data = pycode.attr("idwt")(dwt_data, py::bytes(dwt_structure), "sym13",
-                                                     std::vector<size_t>({100, 500, 500}));
+  py::array_t<float> idwt_data = pywavelet.attr("idwt")(dwt_data, py::bytes(dwt_structure), "sym13",
+                                                        std::vector<size_t>({100, 500, 500}));
   endT = std::chrono::high_resolution_clock::now();
   std::cout << "Time for reverse wavelet: "
             << (std::chrono::duration<double>(endT - startT)).count() << std::endl;
 
   // compare
-  auto stat = sperr::calc_stats(ori_data.data(), idwt_data.data(), ori_data.size(), 1);
+  size_t num = std::accumulate(dims.begin(), dims.end(), static_cast<size_t>(1), std::multiplies<size_t>());
+  auto stat = sperr::calc_stats(data, idwt_data.data(), num, 1);
   printf("Stat: rmse = %f, linfty = %f, psnr = %fdB, orig_min = %f, orig_max = %f\n", stat[0],
          stat[1], stat[2], stat[3], stat[4]);
+}
+
+int main(int argc, char* argv[])
+{
+  std::string HOME = "/Users/kzhao/";
+  py::scoped_interpreter guard{};
+
+  // append source dir to sys.path, and python interpreter would find your custom python file
+  py::module_::import("sys").attr("path").attr("append")(HOME + "/code/sperr/utilities/");
+
+  {
+    printf("\n\nhurricane-100x500x500/Uf48.bin.dat\n");
+    auto data = sperr::read_whole_file<float>(HOME + "/data/hurricane-100x500x500/Uf48.bin.dat");
+    std::vector<size_t> dims({100, 500, 500});
+    wavelet(data.data(), dims);
+  }
+  {
+    printf("\n\nhurricane-100x500x500/Pf48.bin.dat\n");
+    auto data = sperr::read_whole_file<float>(HOME + "/data/hurricane-100x500x500/Pf48.bin.dat");
+    std::vector<size_t> dims({100, 500, 500});
+    wavelet(data.data(), dims);
+  }
+  {
+    printf("\n\nhurricane-100x500x500/Vf48.bin.dat\n");
+    auto data = sperr::read_whole_file<float>(HOME + "/data/hurricane-100x500x500/Vf48.bin.dat");
+    std::vector<size_t> dims({100, 500, 500});
+    wavelet(data.data(), dims);
+  }
+  {
+    printf("\n\nhurricane-100x500x500/Wf48.bin.dat\n");
+    auto data = sperr::read_whole_file<float>(HOME + "/data/hurricane-100x500x500/Wf48.bin.dat");
+    std::vector<size_t> dims({100, 500, 500});
+    wavelet(data.data(), dims);
+  }
 }
